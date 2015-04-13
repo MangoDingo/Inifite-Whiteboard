eddd2bb0f85cc1c84601391b5f5dc55272416dca
Mark Karle <mark.a.karle@vanderbilt.edu>
2015-04-13 17:38:04 -0500

    added body-parser:

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     var p = path.resolve(__dirname + '/Site.html');
 	res.sendFile(p);
 });
 
-
+app.post("/notifyServer", function(req, res) {
+    var path = req.body;
+    res.json(path);
+    console.log(req.body);
+});
 
 var server = app.listen(8888, function (){
 	var host = server.address().address;
