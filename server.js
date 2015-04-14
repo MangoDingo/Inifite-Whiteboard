20d754e4131768eef9b5439040e8df6f5a8babea
Mark Karle <mark.a.karle@vanderbilt.edu>
2015-04-13 20:13:48 -0500

    added some network capabilities. only initiliaztion works, and it's slow

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
+//paperjs project, has whole canvas on it
+var project;
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
@@ -11,14 +13,24 @@ app.get('/', function (req, res) {
 	res.sendFile(p);
 });
 
+//initializes the client with the current whiteboard project
+app.get("/initialize", function(req, res) {
+    console.log(project);
+    res.json(project);
+});
+
+//adds a path to a project (faster)
+//adds updates server's project to client's (slower, currently implemented)
+//i'd prefer to do the faster one
 app.post("/notifyServer", function(req, res) {
-    var path = req.body;
-    res.json(path);
+    project = req.body;
     console.log(req.body);
 });
+
+//TODO update clients about changes
 
 var server = app.listen(8888, function (){
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Server listening at http://%s:%s', host, port);
-})
+});
