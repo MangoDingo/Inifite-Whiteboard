2a85fc54c8cfec8dd33810ea3be27979751e4e44
Mark <markakarle@gmail.com>
2015-04-15 18:29:40 -0500

    added paper dependencies to server. init and real time work but are slow and buggy

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-var express = require('express');
 var path = require('path');
+var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var bodyParser = require('body-parser');
-//paperjs project, has whole canvas on it
-var project;
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
+var paper = require('paper');
+//paperjs drawing, has whole canvas on it
+var extPaper = paper.setup();
+var project = new paper.Project();
+project.layers[0] = new paper.Layer();
+project.layers[0].activate();
+
+server.listen(8888);
+
+//for stylesheets
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
 
+//give the html to the client
+app.get('/', function (req, res) {
+    var p = path.resolve(__dirname + '/Site.html');
+	res.sendFile(p);
+});
+
+//socket io will listen to the socket
 io.on('connection', function(socket){
     console.log('client connected');
+    if(project != undefined){
+        socket.emit('initialization', project.layers[0].exportJSON());
+        console.log('client initialized');
+    }
     
-    socket.on('path update', function(path){
-        
+    //get a new path from a client
+    socket.on('path update', function(path){ 
+        console.log('PATH UPDATE');
+        //tell all the other clients about the path
+        socket.broadcast.emit('path update', path);
+        updateServerCanvas(path);
     });
     
     socket.on('disconnect', function(){
@@ -22,29 +43,8 @@ io.on('connection', function(socket){
     });
 });
 
-app.get('/', function (req, res) {
-    var p = path.resolve(__dirname + '/Site.html');
-	res.sendFile(p);
-});
-
-//initializes the client with the current whiteboard project
-app.get("/initialize", function(req, res) {
-    console.log(project);
-    res.json(project);
-});
-
-//adds a path to a project (faster)
-//adds updates server's project to client's (slower, currently implemented)
-//i'd prefer to do the faster one
-app.post("/notifyServer", function(req, res) {
-    project = req.body;
-    console.log(req.body);
-});
-
-//TODO update clients about changes
-
-var server = app.listen(8888, function (){
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Server listening at http://%s:%s', host, port);
-});
+function updateServerCanvas(path){
+    var newPath = new paper.Path();
+    newPath.importJSON(path);
+    var test = project.layers[0].addChild(newPath);
+}
