ef72c71ee0b03044dc880bf97ec4053cd7c5f267
Mark <markakarle@gmail.com>
2015-04-19 14:52:06 -0500

    uncommented stuff

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-/*var paper = require('paper');
+var paper = require('paper');
 //paperjs drawing, has whole canvas on it
 var extPaper = paper.setup();
 var project = new paper.Project();
 project.layers[0] = new paper.Layer();
 project.layers[0].activate();
-*/
+
 server.listen(8888);
 
 //for stylesheets
@@ -25,11 +25,11 @@ app.get('/', function (req, res) {
 //socket io will listen to the socket
 io.on('connection', function(socket){
     console.log('client connected');
-   /* if(project != undefined){
+   if(project != undefined){
         socket.emit('initialization', project.layers[0].exportJSON());
         console.log('client initialized');
     }
-    */
+    
     socket.on('path update', function(path){
         console.log('PATH UPDATE');
         socket.broadcast.emit('path update', path);
